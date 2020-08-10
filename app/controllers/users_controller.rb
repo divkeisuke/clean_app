45830bbfc0bee8a93d4db00fcb58f2016d3da778
momo <zsewac3221@yahoo.co.jp>
2020-08-10 20:59:30 +0000

    new_login_change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,7 @@ class UsersController < ApplicationController
   def create
      @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = 'ユーザを登録しました。'
       redirect_to root_url
     else
