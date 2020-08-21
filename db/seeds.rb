44bfbe653bb26586f27e52883824e09dfad1cb8e
momo <zsewac3221@yahoo.co.jp>
2020-08-21 07:00:12 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Motivation.create(word:"家の中を整理することは、自分自身を整理すること")
-Motivation.create(word:"ゴキブリの視点から見れば 髪の毛１本、食べかすの１かけらが超巨大な寿司やケーキに等しい")
-Motivation.create(word:"きれいな家に貧乏なし、汚い家に金持ちなし")
-Motivation.create(word:"あなたの住む部屋が、あなた自身である")
+Motivation.create(word:"部屋の乱れは心の乱れ")
+Motivation.create(word:"足元のゴミひとつ拾えぬほどの人間に、何ができましょうか")
+Motivation.create(word:"愛する者、あるいは恋する者をいつ家に入れてもいい入れてもいいように綺麗にしましょう")
+Motivation.create(word:"掃除は心の掃除")
+Motivation.create(word:"一つ拾えば一つ綺麗になる")
+Motivation.create(word:"清潔に勝るインテリアなし")
